dac5943a442e090ec9ca014aac2a6affa06f522d
Karizmaflores <flores_karizma@yahoo.com>
2024-05-13 17:21:50 -0500

    Started skills section

---
+++ src/components/Skills/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { skills } from "../../data/constants";
+import { Description } from "@mui/icons-material";
+
+const Skills = () => {
+    return <Container id="skills">
+        <Wrapper>
+            <Title>Skills</Title>
+            <Desc>Skills I have been working on over my professional years</Desc>
+        </Wrapper>
+    </Container>
+}
+
+export default Skills
